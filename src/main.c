030ea28a97e960de1d9b522dacef45a1f3e70f32
Kataryna Kazinskaya <74289570+hbbtOfLight@users.noreply.github.com>
2022-03-08 20:03:45 +0000

    add task 1

--- src/main.c
+++ src/main.c
@@ -1,8 +1,40 @@
 /* hello.c */
-#include <stdio.h>
+//
+// Created by katar on 08.03.2022.
+//
 
-void main (void)
+#include <stdio.h>
+#include <stdlib.h>
+
+void maximize(int* beg, int size) {
+  for (int i = 0; i < size; ++i) {
+    int counts[10];
+    for (int j = 0; j < 10; ++j) {
+      counts[j] = 0;
+    }
+    while(beg[i] > 0) {
+      ++counts[beg[i] % 10];
+      beg[i] /= 10;
+    }
+    for (int j = 9; j >= 0; --j) {
+      while (counts[j] > 0) {
+        beg[i] *= 10;
+        beg[i] += j;
+        --counts[j];
+      }
+    }
+  }
+}
+int main (int argc, char** argv)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  int* test_arr = (int*)malloc((argc - 1) * sizeof(int));
+  for(int i = 1; i < argc; ++i) {
+    test_arr[i - 1] = atoi(argv[i]);
+  }
+  maximize(test_arr, argc - 1);
+  for (int i = 0; i < argc - 1; ++i) {
+    printf("%i ", test_arr[i]);
+  }
+  
+  free(test_arr);
 }
